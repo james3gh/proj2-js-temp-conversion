19066321f0e4d295147ddd82a4230e2a76115fab
james3gh <jamesthomas3801@gmail.com>
2021-07-24 23:19:00 +0530

    added responsiveness and modified dropdown

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-temp1 = document.querySelector("#temp1");
-temp2 = document.querySelector("#temp2");
+// varaibles 
+var temp1 = document.querySelector("#temp1");
+var temp2 = document.querySelector("#temp2");
+var c1 = document.querySelector("#dropdown1 .C");
+var f1 = document.querySelector("#dropdown1 .F");
+var c2 = document.querySelector("#dropdown2 .C");
+var f2 = document.querySelector("#dropdown2 .F");
 var t1,t2,conversion;
-// console.log(temp1.value);
+
+
+// Eventlistener for temp1 input 
+
 temp1.addEventListener("input",function(){
     if(this.value == "")
         temp2.value = "";
@@ -20,7 +28,7 @@ temp1.addEventListener("input",function(){
         }
         else if(t1 == "Kelvin" && t2 == "Celsius"){
             conversion = Number(this.value) - 273.15;
-            temp2.value = conversion;    
+            temp2.value = conversion.toFixed(3);    
         }
         else if(t1 == "Fahrenheit" && t2 == "Celsius"){
             conversion = ((this.value) - 32)*5/9;
@@ -28,27 +36,16 @@ temp1.addEventListener("input",function(){
         }
         else if(t1 == "Fahrenheit" && t2 == "Kelvin"){
             conversion = ((this.value) - 32)*5/9 + 273.15;
-            temp2.value = conversion.toFixed(3);
+            temp2.value = conversion.toFixed(4);
         }
     }
 })
+
+
+// Eventlistener for temp2 input 
+
 temp2.addEventListener("input",function(){
-    // if(t1 == "Celsius" && t2 == "Fahrenheit"){
-    //     if(this.value == "")
-    //         temp1.value = "";
-    //         else{
-    //         var conversion = (this.value - 32)*5/9;
-    //         temp1.value = conversion.toFixed(3);
-    // }
-    // }
-    // else if(t1 == "Celsius" && t2 == "Kelvin"){
-    //     if(this.value == "")
-    //     temp1.value = "";
-    //     else{
-    //     var conversion = Number(this.value) - 273.15;
-    //     temp1.value = conversion;
-    //     }
-    // }
+
     if(this.value == "")
         temp1.value = "";
     else {
@@ -79,36 +76,50 @@ temp2.addEventListener("input",function(){
     }
 })
 
-//////////////////////////////////////////////////////////
+// function calls
+
 disp();
 function disp(){
 t1 = document.getElementById("dropdown1").value;
-// console.log(t1);
 t2 = document.getElementById("dropdown2").value;
-// console.log(t2);
-// if(t2 == "Kelvin")
-//     temp2.value = "273.15";
-// else if(t1 == "Kelvin")
-//     temp1.value = "273.15";
-// else if(t1 == "Celsius")
-//     temp1.value = 0;
-// else if(t2 == "Celsius")
-//     temp2.value = "0";
-// else if(t1 == "Fahrenheit")
-//     temp1.value = "32";
-// else if(t2 == "Fahrenheit")
-//     temp2.value = "32";
- }
-// var c1 = document.querySelector("#dropdown1 .C");
-// var f1 = document.querySelector("#dropdown1 .F");
-// var k1 = document.querySelector("#dropdown1 .K");
-// var c2 = document.querySelector("#dropdown2 .C");
-// var f2 = document.querySelector("#dropdown2 .F");
-// var k2 = document.querySelector("#dropdown2 .K");
+temp1.value = "";
+temp2.value = "";
+}
 
+// function call for input temp1
+function change1(){
+
+    disp();
+    if(t1 === "Celsius" && t2 === "Celsius"){
+        f2.selected = "true";
+        t2 = "Fahrenheit";
+    }
+    else if(t2 === "Fahrenheit" && t1 === "Fahrenheit"){
+        c2.selected = "true";
+        t2 = "Celsius";
+    }
+    else if(t2 === "Kelvin" && t1 === "Kelvin"){
+        f2.selected = "true";
+        t2 = "Fahrenheit";
+    }
  
-//     if(t1 === "Celsius"){
-//         if(t2 === "Celsius")
-//         f1.selected = "true";
-//     }
-// }
+}
+
+// function call for input temp2
+function change2(){
+    
+    disp();
+    if(t1 === "Celsius" && t2 === "Celsius"){
+        f1.selected = "true";
+        t1 = "Fahrenheit";
+    }
+    else if(t2 === "Fahrenheit" && t1 === "Fahrenheit"){
+        c1.selected = "true";
+        t1 = "Celsius";
+    }
+    else if(t2 === "Kelvin" && t1 === "Kelvin"){
+        f1.selected = "true";
+        t1 = "Fahrenheit";
+    }
+ 
+}
